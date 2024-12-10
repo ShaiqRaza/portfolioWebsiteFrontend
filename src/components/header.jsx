1bc3a200c70f19d5bf3507201359ac03740bbb2f
ShaiqRaza <shaiqraza808y@gmail.com>
2024-12-10 19:00:28 +0500

    update ui

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 const Header = ()=>{
     return(
-        <div className="fixed text-white z-50 font-semibold lg:text-[18px] md:text[17px] sm:text-[16px] xs:text-[15px] text-[14px] flex items-center justify-between lg:px-12 md:px-10 sm:px-9 xs:px-7 xss:px-6 px-5 bg-blue-900 w-full lg:h-[90px] md:h-[85px] sm:h-[80px] xs:h-[70px] xss:h-[65px] h-[60px]">
+        <div className="fixed text-white z-50 font-semibold lg:text-[18px] md:text[17px] sm:text-[16px] xs:text-[15px] text-[14px] flex items-center justify-between lg:px-20 md:px-16 sm:px-14 xs:px-12 xss:px-10 px-8 bg-blue-900 w-full lg:h-[90px] md:h-[85px] sm:h-[80px] xs:h-[70px] xss:h-[65px] h-[60px]">
             <div className='md:gap-[60px] xs:gap-[50px] gap-[40px] flex items-center'>
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/projects'>Projects</NavLink>
