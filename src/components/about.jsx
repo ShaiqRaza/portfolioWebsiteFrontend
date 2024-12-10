af5c269a1b83ed763e9b30cce82d7bc1a1e6b150
ShaiqRaza <shaiqraza808y@gmail.com>
2024-12-10 18:52:28 +0500

    update about ui

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,11 +1,11 @@
 const About = ()=>{
     return(
-        <div className="flex sm:flex-row flex-col justify-center items-center h-[60%] w-full">
-            <div className="flex justify-center items-center sm:w-1/2 w-full">
+        <div className="flex sm:flex-row flex-col justify-center items-center h-[60%] w-full text-white sm:gap-2 gap-4">
+            <div className="flex justify-center items-center sm:w-[40%] w-full sm:order-2 order-1">
                 <img src='/images/khusham.jpg' alt="Image!" className="rounded-full z-20 lg:w-[250px] md:w-[220px] sm:w-[180px] w-[140px] lg:h-[250px] md:h-[220px] sm:h-[180px] h-[140px]"/>
                 <div className="absolute z-10 rounded-full lg:w-[170px] md:w-[150px] sm:w-[110px] w-[90px] lg:h-[170px] md:h-[150px] sm:h-[110px] h-[90px] bg-white animate-pingSlow"></div>
             </div>
-            <div className="sm:w-1/2 w-full">
+            <div className="sm:w-[60%] w-full sm:order-1 order-2">
                 <h1>Hi, i am Muhammad Khusham Ali</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sunt ad explicabo eligendi quasi cumque sequi suscipit voluptatum aspernatur minus, vitae, maxime distinctio modi impedit vel, doloribus at. Ipsa, maxime.
                 </p>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,7 @@
 import About from './about'
 const home = ()=>{
     return (
-        <div className="w-full h-full lg:pt-[90px] md:pt-[85px] sm:pt-[80px] xs:pt-[70px] xss:pt-[65px] pt-[60px] bg-slate-800">
+        <div className="w-full h-full lg:pt-[90px] md:pt-[85px] sm:pt-[80px] xs:pt-[70px] xss:pt-[65px] pt-[60px] bg-slate-800 lg:px-20 md:px-16 sm:px-14 xs:px-12 xss:px-10 px-8">
             <About/>
         </div>
     )
