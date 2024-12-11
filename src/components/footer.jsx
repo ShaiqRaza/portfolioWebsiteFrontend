325af00dfbc64343b39b20fb4a348932a0817138
ShaiqRaza <shaiqraza808y@gmail.com>
2024-12-11 14:23:23 +0500

    Update footer UI

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,32 +6,32 @@ const Footer = () => {
       {/* Copyright Section */}
       <div className="flex items-center mb-4">
         © 2024
-        <a href="#" className="hover:underline ml-1 text-white">
-          muhammadkhushamali
+        <a href="#" className="hover:underline ml-1">
+          MuhammadKhushamAli
         </a>
-        . All Rights Reserved.
+        .  All Rights Reserved.
       </div>
 
       {/* Social Media Links */}
       <div className="flex space-x-6 mb-4">
-        <a href="https://www.facebook.com" className="text-white hover:text-blue-500">
+        <a href="https://www.facebook.com" className=" hover:text-blue-500">
           <FaFacebook size={24} />
         </a>
-        <a href="https://www.twitter.com" className="text-white hover:text-blue-400">
+        <a href="https://www.twitter.com" className=" hover:text-blue-400">
           <FaTwitter size={24} />
         </a>
-        <a href="https://www.instagram.com" className="text-white hover:text-pink-500">
+        <a href="https://www.instagram.com" className=" hover:text-pink-500">
           <FaInstagram size={24} />
         </a>
-        <a href="https://www.linkedin.com" className="text-white hover:text-blue-700">
+        <a href="https://www.linkedin.com" className=" hover:text-blue-700">
           <FaLinkedin size={24} />
         </a>
       </div>
 
       {/* Contact Information */}
       <div className="flex items-center space-x-2">
-        <FaPhoneAlt className="text-white" size={20} />
-        <a href="tel:+1234567890" className="text-white hover:underline">
+        <FaPhoneAlt size={20} />
+        <a href="tel:+1234567890" className=" hover:underline">
           +1 234 567 890
         </a>
       </div>
