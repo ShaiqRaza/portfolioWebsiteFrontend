10d7ba4e00d24f443cf6d34b0fa960ee67a6bb4a
ShaiqRaza <shaiqraza808y@gmail.com>
2024-12-29 18:14:05 +0500

    Update UI

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -28,7 +28,7 @@ const Header = ()=>{
 
     return(
         <div className={
-            `font-semibold lg:text-[18px] md:text[17px] sm:text-[16px] xs:text-[15px] text-[14px] text-white fixed z-50 flex items-center justify-between lg:px-20 md:px-16 sm:px-14 xs:px-12 xss:px-10 px-8 bg-blue-900 w-full ${isScrolled? "lg:h-[90px] md:h-[85px] sm:h-[80px] xs:h-[75px] xss:h-[70px] h-[65px]": "lg:h-[100px] md:h-[95px] sm:h-[90px] xs:h-[85px] xss:h-[80px] h-[75px]"}`
+            `shadow-2xl font-semibold lg:text-[18px] md:text[17px] sm:text-[16px] xs:text-[15px] text-[14px] text-white fixed z-50 flex items-center justify-between lg:px-20 md:px-16 sm:px-14 xs:px-12 xss:px-10 px-8 bg-blue-900 w-full ${isScrolled? "lg:h-[90px] md:h-[85px] sm:h-[80px] xs:h-[75px] xss:h-[70px] h-[65px]": "lg:h-[100px] md:h-[95px] sm:h-[90px] xs:h-[85px] xss:h-[80px] h-[75px]"}`
         }>
             <div className='md:gap-[60px] xs:gap-[35px] sm:gap-[50px] xss:gap-[25px] gap-[20px] flex items-center'>
                 <NavLink 
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -5,7 +5,7 @@ const Skill = ({
     description
 }) => {
     return (
-        <div className="bg-gray-800 p-6 rounded-md shadow-md hover:bg-blue-700 transition-all mb-2 break-inside-avoid">
+        <div className="bg-gray-800 p-6 rounded-md flex-col flex items-center shadow-md hover:bg-blue-700 transition-all mb-2 break-inside-avoid">
             <h3 className="font-bold text-xl mb-3 uppercase text-blue-400">{name}</h3>
             <p className="text-sm text-gray-300 break-words text-left">
                 {description}
@@ -20,7 +20,7 @@ const Skills = () => {
             <h2 className="text-center font-bold text-2xl lg:text-3xl mb-6 text-blue-500 animate__animated animate__fadeIn">My Skills</h2>
             <div className="w-full columns-1 sm:columns-2 lg:columns-3 gap-2">
                 <Skill name="HTML" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sunt ad explicabo eligendi quasi cumque sequi suscipit voluptatum aspernatur minus, vitae, maxime distinctio modi impedit vel, doloribus at. Ipsa, maxime." />
-                <Skill name="CSS" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sunt ad explicabo eligendi quasi cumque sequi suscipit voluptatum aspernatur minus quasi cumque sequi suscipit voluptatum aspernatur minus quasi cumque sequi suscipit voluptatum aspernatur minus quasi cumque sequi suscipit voluptatum aspernatur minus, vitae, maxime distinctio modi impedit vel, doloribus at. Ipsa, maxime." />
+                <Skill name="CSS" description="Lorem ipsum dolor sit amet consectetur adipisicing elit.  suscipit voluptatum aspernatur minus quasi cumque sequi suscipit voluptatum sequi suscipit voluptatum aspernatur minus quasi cumque sequi suscipit voluptatum aspernatur minus, vitae, maxime distinctio modi impedit vel, doloribus at. Ipsa, maxime." />
                 <Skill name="JavaScript" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sunt ad explicabo eligendi impedit vel, doloribus at. Ipsa, maxime." />
                 <Skill name="C++" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sunt ad explicabo eligendi quasi cumque sequi suscipit voluptatum aspernatur minus, vitae, maxime distinctio modi impedit vel, doloribus at. Ipsa, maxime." />
                 <Skill name="DSA" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sunt ad explicabo eligendi quasi cumque sequi suscipit voluptatum aspernatur minus, vitae, modi impedit vel, doloribus at. Ipsa, maxime." />
