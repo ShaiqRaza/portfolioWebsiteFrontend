0b8d49d1a8013a8303790d397cf56d21510ad3d3
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-24 11:59:13 +0500

    about component ui update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -21,10 +21,12 @@ const About = () => {
 
 
     return (
-        <div className="flex sm:flex-row flex-col justify-evenly items-center h-[80vh] w-full text-white sm:gap-2">
-            <div className="flex justify-center items-center sm:w-[40%] w-full sm:order-2 order-1 relative">
+        <div className="flex sm:flex-row flex-col justify-evenly items-center h-[80vh] w-full text-white sm:gap-2 px-[5vw]">
+            <div className="flex justify-center sm:justify-end items-center sm:w-[40%] w-full sm:order-2 order-1 relative">
                 <img src={about?.avatar} alt="Image!" className="rounded-full z-20 lg:w-[350px] md:w-[270px] sm:w-[230px] w-[180px] lg:h-[350px] md:h-[270px] sm:h-[230px] h-[180px]" />
-                <div className="absolute z-10 rounded-full lg:w-[220px] md:w-[170px] sm:w-[150px] w-[120px] lg:h-[220px] md:h-[170px] sm:h-[150px] h-[120px] bg-white animate-pingSlow"></div>
+                <div className='absolute lg:w-[350px] md:w-[270px] sm:w-[230px] w-[180px] lg:h-[350px] md:h-[270px] sm:h-[230px] h-[180px] flex justify-center items-center'>
+                    <div className="absolute z-10 rounded-full lg:w-[220px] md:w-[170px] sm:w-[150px] w-[120px] lg:h-[220px] md:h-[170px] sm:h-[150px] h-[120px] bg-white animate-pingSlow"></div>
+                </div>
             </div>
             <div className="sm:w-[60%] w-full sm:order-1 order-2">
                 <h1 className="text-2xl sm:text-3xl font-bold text-white">{about?.intro}</h1>
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -16,8 +16,6 @@ const Projects = ()=>{
             )
         })
     }, [])
-
-    console.log(projects)
     
     return (
         <div className="w-full h-[80vh] lg:pt-[90px] md:pt-[85px] sm:pt-[80px] xs:pt-[70px] xss:pt-[65px] pt-[60px] bg-gray-900 lg:px-20 md:px-16 sm:px-14 xs:px-12 xss:px-10 px-8">
