4ba220143eb84b01e272b8d8dbf8337a274140e2
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-24 09:55:44 +0500

    update login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,10 +2,13 @@ import {useState} from 'react'
 const Login = ()=>{
     const [ishovered, sethovered] = useState(false)
 
-
+    const handleSubmission = (e)=>{
+        e.preventDefault();
+        console.log(e)
+    }
 
     return (
-        <div className="w-full h-[80vh] bg-gray-900 lg:px-20 md:px-16 sm:px-14 xs:px-12 xss:px-10 px-8 flex justify-center items-center">       
+        <div className="w-full h-[80vh] lg:pt-[100px] md:pt-[95px] sm:pt-[90px] xs:pt-[85px] xss:pt-[80px] pt-[75px] bg-gray-900 lg:px-20 md:px-16 sm:px-14 xs:px-12 xss:px-10 px-8 flex justify-center items-center">       
             <form
             onMouseEnter={
                 ()=>{
@@ -17,12 +20,16 @@ const Login = ()=>{
                     sethovered(false);
                 }
             } 
-            className="flex text-white"
+            className="flex lg:gap-3 gap-4 flex-col justify-center items-center text-white"
+
+            onSubmit = {handleSubmission}
+
             >
-                <input type="text" placeholder="Enter Password" className="border-b-2 bg-gray-900 outline-none sm:h-[35px] sm:text-[16px] h-[30px] border-white hover:border-cyan-400 w-[200px]"/>
+                <input type="email" placeholder="Enter Email" className="pl-1 border-b-2 bg-gray-900 outline-none sm:h-[35px] sm:text-[16px] h-[30px] border-white hover:border-cyan-500 w-[200px]"/>
+                <input type="text" placeholder="Enter Password" className="pl-1 border-b-2 bg-gray-900 outline-none sm:h-[35px] sm:text-[16px] h-[30px] border-white hover:border-cyan-500 w-[200px]"/>
                 <button type="submit" 
                 className={
-                    `${ishovered? "block":"hidden"} sm:h-[35px] sm:w-[45px] h-[30px] w-[37px] outline-none hover:bg-cyan-500 bg-cyan-400`
+                    `${ishovered? "block":"hidden"} sm:h-[30px] sm:w-[45px] h-[25px] w-[37px] outline-none hover:bg-cyan-600 bg-cyan-500 rounded-[7px] font-semibold`
                 }
                 >Go</button>
             </form>    
