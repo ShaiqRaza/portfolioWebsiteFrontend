7ced86b4f8c8f1a62e35d45ec9a7e79030f43640
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-24 10:45:45 +0500

    setup project component

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 
 const Projects = ()=>{
-    const [obj, setObj] = useState(new link("ye mera github hai: ", "https://github.com/ShaiqRaza"))
+    
     return (
         <div className="w-full lg:pt-[90px] md:pt-[85px] sm:pt-[80px] xs:pt-[70px] xss:pt-[65px] pt-[60px] bg-gray-900 lg:px-20 md:px-16 sm:px-14 xs:px-12 xss:px-10 px-8 min-h-[80vh]">
             
