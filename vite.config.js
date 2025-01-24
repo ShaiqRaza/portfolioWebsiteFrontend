a5dff722e91c9e344c4fcbb61353164f88278b3a
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-25 00:29:55 +0500

    remove issues in redundant code

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_BACKEND_URL': process.env.VITE_BACKEND_URL,
+    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL),
   },
 });
