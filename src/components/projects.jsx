a6021f95151c4242e8c088d3d74e2cafd4f91bc1
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-25 17:08:40 +0500

    update project component

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,9 +8,33 @@ const Project = ({project})=>{
     const [clicked, setClicked] = useState(false);
 
     return(
-        <div onClick={()=>{setClicked(!clicked)}} key={project._id} className='cursor-pointer rounded-sm h-[7vh] bg-slate-700 text-white font-bold text-xl px-3 flex items-center justify-between'>
-            <div>{project.title}</div>
-            {clicked? <IoIosArrowDown size={16}/>: <IoIosArrowForward size={16}/>}
+        <div onClick={()=>{setClicked(!clicked)}} key={project._id} className='rounded-sm bg-slate-700 text-white px-3'>
+            <div className='capitalize cursor-pointer h-[7vh] flex items-center justify-between font-bold text-xl'>
+                <div>{project.title}</div>
+                {clicked? <IoIosArrowDown size={16}/>: <IoIosArrowForward size={16}/>}
+            </div>
+            {
+                clicked &&
+                <div className='flex flex-col gap-3 px-3'>
+                    <p>{project.description}</p>
+                    <div className='w-full columns-1 sm:columns-2 lg:columns-3 gap-2'>
+                        {
+                            images?.length>0 &&
+                            images.map(image => {
+                                return (
+                                    <img src={image.image} key={_id} className='h-max-[40vh] w-max-[80vw] h-auto'/>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className='w-full justify-center py-5'>
+                        {
+                            video &&
+                            <video src="project.video"></video>
+                        }
+                    </div>
+                </div>
+            }
         </div>
     )
 }
