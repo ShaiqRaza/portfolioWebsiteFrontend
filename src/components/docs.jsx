0f1f597f7e29e3b5da22fa4c7c8fc35622af2f0a
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-31 09:17:14 +0500

    update docs controller

--- src/components/docs.jsx
+++ src/components/docs.jsx
@@ -7,10 +7,10 @@ const breakpointColumns = {
     400: 2
 }
 
-const Doc = ({document}) =>{
+const Doc = ({document, setImageClicked}) =>{
     return (
         <div className="py-2 flex flex-col gap-2">
-            <img src={document.image} alt="Image!" className="max-h-[30vh] min-h-[10vh] h-auto"/>
+            <img src={document.image} alt="Image!" className="max-h-[70vh] min-h-[10vh] h-auto" onClick={()=>{setImageClicked(document)}}/>
             <p className="text-white font-bold text-xl">{document.title}</p>
         </div>
     )
@@ -19,6 +19,7 @@ const Doc = ({document}) =>{
 const Docs = ()=>{
 
     const [docs, setDocs] = useState(null);
+    const [imageClicked, setImageClicked] = useState(null);
 
     useEffect(()=>{
         axios.get(`${import.meta.env.VITE_BACKEND_URL}/doc/get-all`)
@@ -33,15 +34,26 @@ const Docs = ()=>{
     }, [])
 
     return (
-        <Masonry className="px-[5vw] w-full min-h-[80vh] h-auto lg:pt-[17vh] sm:pt-[16vh] pt-[13vh] bg-gray-900 flex gap-2" breakpointCols={breakpointColumns}>
-            {
-                docs?.map(doc=>{
-                    return (
-                        <Doc document={doc} key={doc._id}/>
-                    )
-                })
+        <>  {
+                imageClicked &&
+                <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 z-50">
+                    <div className="flex flex-col gap-4 justify-center items-center w-auto h-full">
+                        <button onClick={()=>{setImageClicked(null)}} className="absolute top-0 right-0 text-2xl p-2 text-gray-500 hover:text-white mr-2">X</button>
+                        <img src={imageClicked.image} alt="Image!" className="max-h-[70vh] max-w-[60vw]"/>
+                        <div className="max-w-[60vw] max-h-[20vh] text-white">{imageClicked.description}</div>
+                    </div>
+                </div>
             }
-        </Masonry>
+            <Masonry className="px-[5vw] w-full min-h-[80vh] h-auto lg:pt-[17vh] sm:pt-[16vh] pt-[13vh] bg-gray-900 flex gap-2" breakpointCols={breakpointColumns}>
+                {
+                    docs?.map(doc=>{
+                        return (
+                            <Doc document={doc} key={doc._id} setImageClicked={setImageClicked}/>
+                        )
+                    })
+                }
+            </Masonry>
+        </>
     )
 }
 
