c6f3c6e59b0755048dff3fb2afcbfe14ca7378ea
ShaiqRaza <shaiqraza808y@gmail.com>
2025-01-31 08:51:11 +0500

    add masonry layout to docs

--- src/components/docs.jsx
+++ src/components/docs.jsx
@@ -1,10 +1,17 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
+import Masonry from 'react-masonry-css'
+const breakpointColumns = {
+    default: 4,
+    580: 3,
+    400: 2
+}
 
 const Doc = ({document}) =>{
     return (
-        <div className="w-full columns-2 sm:columns-3 lg:columns-4 gap-1">
-            
+        <div className="py-2 flex flex-col gap-2">
+            <img src={document.image} alt="Image!" className="max-h-[30vh] min-h-[10vh] h-auto"/>
+            <p className="text-white font-bold text-xl">{document.title}</p>
         </div>
     )
 }
@@ -16,7 +23,7 @@ const Docs = ()=>{
     useEffect(()=>{
         axios.get(`${import.meta.env.VITE_BACKEND_URL}/doc/get-all`)
         .then((response)=>{
-            setDocs(response)
+            setDocs(response.data.data)
         })
         .catch((err)=>{
             return (
@@ -26,7 +33,15 @@ const Docs = ()=>{
     }, [])
 
     return (
-        <div className="px-[5vw] w-full min-h-[80vh] h-auto lg:pt-[17vh] sm:pt-[16vh] pt-[13vh] bg-gray-900 flex flex-col gap-1"></div>
+        <Masonry className="px-[5vw] w-full min-h-[80vh] h-auto lg:pt-[17vh] sm:pt-[16vh] pt-[13vh] bg-gray-900 flex gap-2" breakpointCols={breakpointColumns}>
+            {
+                docs?.map(doc=>{
+                    return (
+                        <Doc document={doc} key={doc._id}/>
+                    )
+                })
+            }
+        </Masonry>
     )
 }
 
