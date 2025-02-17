2a854bee8c1dc50b0873bb9368ae350fa9e24673
ShaiqRaza <shaiqraza808y@gmail.com>
2025-02-17 13:24:51 +0500

    update crud side of about component

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,15 +1,19 @@
 import axios from 'axios';  
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { MdOutlineModeEdit } from "react-icons/md";
 
 const About = ({isLogged}) => {
     
     const [about, setAbout] = useState(null);
+    const [intro, setIntro] = useState(null);
+    const [description, setDescription] = useState(null);
 
     useEffect(()=>{
         axios.get(`${import.meta.env.VITE_BACKEND_URL}/about/get`)
         .then((response)=>{
             setAbout(response.data.data);
+            setIntro(response.data.data.intro);
+            setDescription(response.data.data.description);
         })
         .catch((err)=>{
             return (
@@ -22,7 +26,7 @@ const About = ({isLogged}) => {
         <div className="flex sm:flex-row flex-col justify-evenly items-center h-[80vh] w-full text-white sm:gap-2">
             <div className="flex justify-center sm:justify-end items-center sm:w-[40%] w-full sm:order-2 order-1 relative">
                 <div className="relative z-20 lg:w-[350px] md:w-[270px] sm:w-[230px] w-[180px] lg:h-[350px] md:h-[270px] sm:h-[230px] h-[180px]">
-                    <img src={about?.avatar} alt="Image!" className='w-full h-full rounded-full z-20' />
+                    <img src={about?.avatar} alt="Image!" className='w-full h-full rounded-full z-20'/>
                     {
                         isLogged && <MdOutlineModeEdit className='cursor-pointer absolute sm:right-[12%] right-[10%] bottom-3 rounded-full hover:bg-sky-500 bg-gray-700 lg:w-[40px] md:w-[35px] sm:w-[30px] w-[25px] lg:h-[40px] md:h-[35px] sm:h-[30px] h-[25px] md:p-2 p-1'/>
                     }
@@ -33,19 +37,19 @@ const About = ({isLogged}) => {
             </div>
             <div className="sm:w-[60%] w-full sm:order-1 order-2">
                 <div className='flex items-center gap-[2vw] relative '>
-                    <h1 className="text-2xl sm:text-3xl font-bold text-white ">{about?.intro}</h1>
                     {
-                        isLogged &&
-                        isLogged && <MdOutlineModeEdit className='cursor-pointer rounded-full hover:bg-sky-500 bg-gray-700 lg:w-[35px] md:w-[30px] sm:w-[25px] w-[20px] lg:h-[35px] md:h-[30px] sm:h-[25px] h-[20px] md:p-2 p-1'/>
+                        isLogged
+                        ?<input type="text" value={intro} className='bg-gray-900 text-2xl sm:text-3xl font-bold text-white outline-none' onChange={(e)=>{setIntro(e.target.value)}}/>
+                        :<h1 className="text-2xl sm:text-3xl font-bold text-white ">{about?.intro}</h1>
                     }
                 </div>
-                <p className="text-gray-300 mt-2 sm:text-lg text-sm flex gap-[2vw]">
-                    {about?.description}
+                <div className='flex items-center gap-[2vw] relative text-gray-300'>
                     {
-                        isLogged &&
-                        isLogged && <MdOutlineModeEdit className='cursor-pointer rounded-full hover:bg-sky-500 bg-gray-700 lg:w-[30px] md:w-[25px] sm:w-[20px] w-[15px] lg:h-[30px] md:h-[25px] sm:h-[20px] h-[15px] md:p-2 p-1'/>
+                        isLogged
+                        ?<textarea value={description} className='bg-gray-900 w-full text-sm sm:text-base outline-none resize-none' onChange={(e)=>{setDescription(e.target.value)}}/>
+                        :<p className="text-sm sm:text-base">{about?.description}</p>
                     }
-                </p>
+                    </div>
             </div>
         </div>
     );
