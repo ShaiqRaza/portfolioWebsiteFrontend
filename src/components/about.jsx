cb75d9c3faf355b6bd4567674c4396af0e6a6df5
ShaiqRaza <shaiqraza808y@gmail.com>
2025-02-21 17:40:05 +0500

    update frontend of about component

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -40,20 +40,20 @@ const About = ({isLogged}) => {
     }
 
     return (
-        <div className="flex sm:flex-row flex-col justify-evenly items-center h-[80vh] w-full text-white sm:gap-2">
+        <div className="flex sm:flex-row flex-col justify-evenly items-center h-[80vh] w-full text-white xl:gap-5 md:gap-3 sm:gap-2">
             <div className="flex justify-center sm:justify-end items-center sm:w-[40%] w-full sm:order-2 order-1 relative">
-                <div className="relative z-20 lg:w-[350px] md:w-[270px] sm:w-[230px] w-[180px] lg:h-[350px] md:h-[270px] sm:h-[230px] h-[180px]">
+                <div className="relative z-20 xl:w-[450px] lg:w-[370px] md:w-[270px] sm:w-[220px] w-[200px] xl:h-[450px] lg:h-[370px] md:h-[270px] sm:h-[220px] h-[200px]">
                     <img src={avatar} alt="Image!" className='w-full h-full rounded-full z-20'/>
                     {
                         isLogged &&
                             <>
                                 <label htmlFor="imageID"><MdOutlineModeEdit className='cursor-pointer absolute sm:right-[12%] right-[10%] bottom-3 rounded-full hover:bg-sky-500 bg-gray-700 lg:w-[40px] md:w-[35px] sm:w-[30px] w-[25px] lg:h-[40px] md:h-[35px] sm:h-[30px] h-[25px] md:p-2 p-1'/></label>
-                                <input id='imageID' type="file" name='avatar' className='hidden' onChange={(e)=>{handleAvatarSubmission(e)}}/>
+                                <input id='imageID' type="file" accept='image/*' name='avatar' className='hidden' onChange={(e)=>{handleAvatarSubmission(e)}}/>
                             </>
                     }
                 </div>
-                <div className='absolute lg:w-[350px] md:w-[270px] sm:w-[230px] w-[180px] lg:h-[350px] md:h-[270px] sm:h-[230px] h-[180px] flex justify-center items-center'>
-                    <div className="absolute z-10 rounded-full lg:w-[220px] md:w-[170px] sm:w-[150px] w-[120px] lg:h-[220px] md:h-[170px] sm:h-[150px] h-[120px] bg-white animate-pingSlow"></div>
+                <div className='absolute xl:w-[450px] lg:w-[370px] md:w-[270px] sm:w-[220px] w-[200px] xl:h-[450px] lg:h-[370px] md:h-[270px] sm:h-[220px] h-[200px] flex justify-center items-center'>
+                    <div className="absolute z-10 rounded-full xl:w-[280px] lg:w-[240px] md:w-[180px] sm:w-[140px] w-[140px] xl:h-[280px] lg:h-[240px] md:h-[180px] sm:h-[140px] h-[140px] bg-white animate-pingSlow"></div>
                 </div>
             </div>
             <div className="sm:w-[60%] w-full sm:order-1 order-2 flex flex-col gap-[2vw]">
