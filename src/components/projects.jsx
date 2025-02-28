f8c29a54e085c85037bcf69e4a70756af214acd9
ShaiqRaza <shaiqraza808y@gmail.com>
2025-02-28 10:56:07 +0500

    update projects component

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -205,52 +205,56 @@ const Projects = ()=>{
             {
                 projects?
                 <div>
-                {
-                    addProject &&
-                    <form onSubmit={addProjectSubmission} className={`fixed w-full h-full flex justify-center items-center bg-black bg-opacity-80 z-50`}>
-                        <div className='sm:w-[450px] w-[80%] bg-gray-800 p-6 rounded-md flex flex-col gap-3'>
-                            <h2 className='text-2xl text-sky-500 font-bold'>Add a new Project</h2>
-                            <input type="text" placeholder='Title' required className='outline-none w-full bg-gray-700 text-white p-2 rounded-md'/>
-                            <textarea placeholder='Description' required className='outline-none w-full bg-gray-700 text-white p-2 rounded-md'></textarea>
-                            <div className='flex gap-2'>
-                                <label htmlFor="project-images" className='w-1/2 bg-gray-700 text-white sm:text-base text-sm flex justify-center rounded-md p-1 cursor-pointer hover:text-cyan-500'>Add Images</label>
-                                <input id='project-images' type="file" accept='image/*' multiple className='w-full bg-gray-700 text-white p-2 rounded-md hidden'/>
-                                <label htmlFor="project-video" className='w-1/2 bg-gray-700 text-white sm:text-base text-sm flex justify-center rounded-md p-1 cursor-pointer hover:text-cyan-500'>Add Video</label>
-                                <input id='project-video' type="file" accept='video/*' className='w-full bg-gray-700 text-white p-2 rounded-md hidden'/>
-                            </div>
-                            <button type='submit' className='w-full bg-sky-500 text-white p-2 rounded-md hover:bg-sky-600'>Add</button>
-                            <button onClick={()=>{setAddProject(false)}} className='w-full bg-red-500 text-white p-2 rounded-md hover:bg-red-600'>Cancel</button>
-                        </div>
-                    </form>
-                }
-                {
-                    imageClicked &&
-                    <div className={`fixed w-full h-full flex justify-center items-center bg-black bg-opacity-80 z-50`}>
-                        <button className='absolute sm:top-2 right-4 top-4 text-gray-500 hover:text-white text-2xl' onClick={()=>{setImageClicked(null)}}>X</button>
-                        <img src={imageClicked} alt="Image!" className='rounded-md drop-shadow-lg max-h-[70vh] sm:max-w-[60vw] max-w-[90vw]' />
-                    </div>
-                }
-                <div className={`px-[5vw] w-full min-h-[80vh] lg:pt-[17vh] sm:pt-[16vh] pt-[13vh] bg-gray-900 flex flex-col gap-1 z-0`}>
-                    {
-                        isLogged &&
-                        <div className='w-full flex justify-center mb-2'>
-                            <h1 onClick={()=>{setAddProject(true)}} className='sm:text-sm text-xs text-white cursor-pointer hover:text-cyan-500'>Add project</h1>
-                        </div>
-                    }
                     {
                         projects.length>0 ?
-                        projects.map(project=>{
-                            return (
-                                <Project isLogged={isLogged} handleVideoAddition={handleVideoAddition} handleVideoDeletion={handleVideoDeletion} handleImageDeletion={handleImageDeletion} handleAddImage={handleAddImage} handleDiscriptionUpdation={handleDiscriptionUpdation} handleTitleUpdation={handleTitleUpdation} handleProjectDeletion={handleProjectDeletion} project={project} setImageClicked={setImageClicked} key={project._id}/>                        
-                            )
-                        })
+                        <div>
+                            {
+                            addProject &&
+                            <form onSubmit={addProjectSubmission} className={`fixed w-full h-full flex justify-center items-center bg-black bg-opacity-80 z-50`}>
+                            <div className='sm:w-[450px] w-[80%] bg-gray-800 p-6 rounded-md flex flex-col gap-3'>
+                                <h2 className='text-2xl text-sky-500 font-bold'>Add a new Project</h2>
+                                <input type="text" placeholder='Title' required className='outline-none w-full bg-gray-700 text-white p-2 rounded-md'/>
+                                <textarea placeholder='Description' required className='outline-none w-full bg-gray-700 text-white p-2 rounded-md'></textarea>
+                                <div className='flex gap-2'>
+                                    <label htmlFor="project-images" className='w-1/2 bg-gray-700 text-white sm:text-base text-sm flex justify-center rounded-md p-1 cursor-pointer hover:text-cyan-500'>Add Images</label>
+                                    <input id='project-images' type="file" accept='image/*' multiple className='w-full bg-gray-700 text-white p-2 rounded-md hidden'/>
+                                    <label htmlFor="project-video" className='w-1/2 bg-gray-700 text-white sm:text-base text-sm flex justify-center rounded-md p-1 cursor-pointer hover:text-cyan-500'>Add Video</label>
+                                    <input id='project-video' type="file" accept='video/*' className='w-full bg-gray-700 text-white p-2 rounded-md hidden'/>
+                                </div>
+                                <button type='submit' className='w-full bg-sky-500 text-white p-2 rounded-md hover:bg-sky-600'>Add</button>
+                                <button onClick={()=>{setAddProject(false)}} className='w-full bg-red-500 text-white p-2 rounded-md hover:bg-red-600'>Cancel</button>
+                            </div>
+                            </form>
+                            }
+                            {
+                                imageClicked &&
+                                <div className={`fixed w-full h-full flex justify-center items-center bg-black bg-opacity-80 z-50`}>
+                                <button className='absolute sm:top-2 right-4 top-4 text-gray-500 hover:text-white text-2xl' onClick={()=>{setImageClicked(null)}}>X</button>
+                                <img src={imageClicked} alt="Image!" className='rounded-md drop-shadow-lg max-h-[70vh] sm:max-w-[60vw] max-w-[90vw]' />
+                                </div>
+                            }
+                            <div className={`px-[5vw] w-full min-h-[80vh] lg:pt-[17vh] sm:pt-[16vh] pt-[13vh] bg-gray-900 flex flex-col gap-1 z-0`}>
+                            {
+                                isLogged &&
+                                <div className='w-full flex justify-center mb-2'>
+                                    <h1 onClick={()=>{setAddProject(true)}} className='sm:text-sm text-xs text-white cursor-pointer hover:text-cyan-500'>Add project</h1>
+                                </div>
+                            }
+                            {
+                                projects.map(project=>{
+                                    return (
+                                        <Project isLogged={isLogged} handleVideoAddition={handleVideoAddition} handleVideoDeletion={handleVideoDeletion} handleImageDeletion={handleImageDeletion} handleAddImage={handleAddImage} handleDiscriptionUpdation={handleDiscriptionUpdation} handleTitleUpdation={handleTitleUpdation} handleProjectDeletion={handleProjectDeletion} project={project} setImageClicked={setImageClicked} key={project._id}/>                        
+                                    )
+                                })
+                            }
+                            </div>
+                        </div>
                         :
-                        <p className='text-white self-center justify-self-center'>I have <span className='text-cyan-500'>no projects</span> yet.</p>
+                        <p className='lg:pt-[17vh] sm:pt-[16vh] pt-[13vh] h-[80vh] w-full flex justify-center items-center text-white'>I have <span className='text-cyan-500'>no projects</span> yet.</p>
                     }
                 </div>
-                </div>
                 :
-                <div className='h-[80vh] w-full flex justify-center items-center bg-gray-900'>
+                <div className='lg:pt-[17vh] sm:pt-[16vh] pt-[13vh] h-[80vh] w-full flex justify-center items-center bg-gray-900'>
                     <Box>
                         <CircularProgress />
                     </Box>
