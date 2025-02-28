fdf9e446646ebb845aec24bef6fd5a96ae3b17e9
ShaiqRaza <shaiqraza808y@gmail.com>
2025-02-28 11:38:46 +0500

    update header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -45,7 +45,7 @@ const Header = ({
     return(
         <div className='w-full flex justify-center'>
             <div className={
-                `mt-[30px] left-8 right-8 w-full rounded-md shadow-2xl font-semibold lg:text-[18px] md:text[17px] sm:text-[16px] xs:text-[15px] text-[14px] text-white fixed z-50 flex items-center justify-between bg-blue-900 ${isScrolled? "sm:h-[11vh] h-[9vh]": "sm:h-[12vh] h-[10vh]"}`
+                `lg:mt-[3vh] sm:mt-[2vh] mt-[1vh] w-[92vw] px-[4vw] rounded-md shadow-2xl font-semibold lg:text-[18px] md:text[17px] sm:text-[16px] xs:text-[15px] text-[14px] text-white fixed z-50 flex items-center justify-between bg-blue-900 ${isScrolled? "sm:h-[11vh] h-[9vh]": "sm:h-[12vh] h-[10vh]"}`
             }>
                 <div className='md:gap-[60px] xs:gap-[35px] sm:gap-[50px] xss:gap-[25px] gap-[20px] flex items-center'>
                     <NavLink 
@@ -83,7 +83,7 @@ const Header = ({
                         <FontAwesomeIcon icon={faPen} className='cursor-pointer lg:text-[20px] md:text[19px] sm:text-[18px] xs:text-[17px] text-[16px]' />
                     </NavLink> 
                 </div>
-            </div>
+                </div>
         </div>
     )
 }
