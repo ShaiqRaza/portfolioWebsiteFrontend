e073a18ac6d02d8bbaf63dd4f3244659a28f4e3c
ShaiqRaza <shaiqraza808y@gmail.com>
2025-06-05 11:02:12 +0500

    font update -6

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,7 +7,7 @@ const home = ()=>{
     const { isLogged } = useOutletContext();
 
     return (
-        <div className="w-full px-[4vw] lg:pt-[15vh] sm:pt-[14vh] pt-[11vh] bg-gray-900 font-noteFont tracking-[0.2em]">
+        <div className="w-full px-[4vw] lg:pt-[15vh] sm:pt-[14vh] pt-[11vh] bg-gray-900 font-noteFont tracking-[0.17em]">
             <About isLogged={isLogged}/>
             <Skills isLogged={isLogged}/>
             <Contact isLogged={isLogged}/>
