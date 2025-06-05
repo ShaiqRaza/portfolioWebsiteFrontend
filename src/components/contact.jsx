56f908d00a0b01af4c3e2ff461a4c2eac9764e1b
ShaiqRaza <shaiqraza808y@gmail.com>
2025-06-05 11:56:58 +0500

    update contact me UI

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -28,46 +28,54 @@ const Contact = ({isLogged}) => {
                 ? <h1 className='mb-6 text-xl'>Thank you for <span className='text-cyan-500'>contacting</span> me.</h1>
                 : <>
                     <h2 className="text-center font-bbbFont text-3xl sm:text-4xl lg:text-5xl mb-2 text-cyan-500">Contact Me</h2>
-                    <form className={`w-full bg-gray-800 lg:p-8 md:p-6 sm:p-5 p-4 rounded-lg`} onSubmit={handleSubmit}>
-                        <div className='grid sm:grid-cols-2 gap-4 pb-4'>
-                            <div>
-                                <label htmlFor="name" className="block lg:text-base sm:text-sm text-xs font-semibold text-white">Your Name</label>
-                                <input
-                                    type="text"
-                                    id="name"
-                                    name="name"
-                                    required
-                                    className="w-full sm:px-3 px-2 sm:py-2 py-1 mt-2 bg-transparent border-2 border-white hover:border-cyan-400 outline-none rounded-lg text-white transition-all"
-                                />
-                            </div>
+                    <div className='w-full rotate-1 rounded-lg border border-cyan-500'>
+                        <div className='w-full -rotate-2 rounded-lg border border-cyan-500'>
+                            <div className='w-full -rotate-1 rounded-lg border border-cyan-500'>
+                                <div className='w-full rotate-[4deg] border border-cyan-500 rounded-lg'>
+                                    <form className={`w-full bg-gray-800 lg:p-8 md:p-6 sm:p-5 p-4 rounded-lg -rotate-2`} onSubmit={handleSubmit}>
+                                        <div className='grid sm:grid-cols-2 gap-4 pb-4'>
+                                                <div>
+                                                    <label htmlFor="name" className="block lg:text-base sm:text-sm text-xs font-semibold text-white">Your Name</label>
+                                                    <input
+                                                        type="text"
+                                                        id="name"
+                                                        name="name"
+                                                        required
+                                                        className="w-full sm:px-3 px-2 sm:py-2 py-1 mt-2 bg-transparent border-2 border-white hover:border-cyan-400 outline-none rounded-lg text-white transition-all"
+                                                    />
+                                                </div>
 
-                            <div>
-                                <label htmlFor="email" className="block lg:text-base sm:text-sm text-xs font-semibold text-white">Email Address</label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    name="email"
-                                    required
-                                    className="w-full sm:px-3 px-2 sm:py-2 py-1 mt-2 bg-transparent border-2 border-white rounded-lg text-white hover:border-cyan-400 outline-none transition-all"
-                                />
+                                                <div>
+                                                    <label htmlFor="email" className="block lg:text-base sm:text-sm text-xs font-semibold text-white">Email Address</label>
+                                                    <input
+                                                        type="email"
+                                                        id="email"
+                                                        name="email"
+                                                        required
+                                                        className="w-full sm:px-3 px-2 sm:py-2 py-1 mt-2 bg-transparent border-2 border-white rounded-lg text-white hover:border-cyan-400 outline-none transition-all"
+                                                    />
+                                                </div>
+                                            </div>
+
+                                            <div className="mb-6">
+                                                <label htmlFor="message" className="block lg:text-base sm:text-sm text-xs font-semibold text-white">Your Message</label>
+                                                <textarea
+                                                    id="message"
+                                                    name="message"
+                                                    required
+                                                    className="w-full custom-scrollbar sm:px-3 px-2 sm:py-2 py-1 mt-2 bg-transparent border-2 border-white rounded-lg text-white hover:border-cyan-400 outline-none transition-all"
+                                                    rows="3"
+                                                />
+                                            </div>
+
+                                            <button type="submit" className={`w-full bg-cyan-700 hover:bg-cyan-800 text-white p-4 rounded-lg font-semibold transform transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500`}>
+                                                Send Message
+                                            </button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-
-                        <div className="mb-6">
-                            <label htmlFor="message" className="block lg:text-base sm:text-sm text-xs font-semibold text-white">Your Message</label>
-                            <textarea
-                                id="message"
-                                name="message"
-                                required
-                                className="w-full custom-scrollbar sm:px-3 px-2 sm:py-2 py-1 mt-2 bg-transparent border-2 border-white rounded-lg text-white hover:border-cyan-400 outline-none transition-all"
-                                rows="3"
-                            />
-                        </div>
-
-                        <button type="submit" className={`w-full bg-cyan-700 hover:bg-cyan-800 text-white p-4 rounded-lg font-semibold transform transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500`}>
-                            Send Message
-                        </button>
-                    </form>
+                    </div>
                 </>
             }
         </div>
